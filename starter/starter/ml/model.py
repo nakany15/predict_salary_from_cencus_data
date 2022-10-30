248bc53c01e670c4c38587ae3cbc97b67b522215
Yusuke Nakano <nakany@YusukenoMacBook-Air.local>
2022-10-30 18:14:14 +0900

    updated starter codes

--- starter/starter/ml/model.py
+++ starter/starter/ml/model.py
@@ -1,5 +1,5 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
-
+from sklearn.linear_model import LogisticRegression
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -17,8 +17,9 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
-
-    pass
+    clf = LogisticRegression()
+    clf.fit(X_train, y_train)
+    return clf
 
 
 def compute_model_metrics(y, preds):
@@ -57,4 +58,4 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    pass
+    return model.predict(X)
--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -3,12 +3,18 @@
 from sklearn.model_selection import train_test_split
 
 # Add the necessary imports for the starter code.
+import pandas as pd
+from ml.data import process_data
+from ml.model import train_model
+import pickle
 
 # Add code to load in the data.
-
+data = pd.read_csv('starter/data/cencus.csv')
+data.columns = [col.strip() for col in data.columns]
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
 
+# Proces the test data with the process_data function.
 cat_features = [
     "workclass",
     "education",
@@ -23,6 +29,8 @@ X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
 
-# Proces the test data with the process_data function.
 
 # Train and save a model.
+model = train_model(X_train, y_train)
+with open('starter/model/model.pkl', 'wb') as f:
+    pickle.dump(model, f)
