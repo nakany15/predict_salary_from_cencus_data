404603ce001ca0261d78f8233e9b6bd2f75b62b4
Yusuke Nakano <nakany@YusukenoMacBook-Air.local>
2022-11-07 20:42:54 +0900

    modified import bug

--- starter/tests/test_api.py
+++ starter/tests/test_api.py
@@ -7,6 +7,10 @@ from fastapi.testclient import TestClient
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
 
+@pytest.fixture(scope="module")
+def test_app():
+    client = TestClient(app)
+    return client
 
 def test_greet(test_app):
     response = test_app.get("/")
@@ -14,10 +18,7 @@ def test_greet(test_app):
     assert response.json()["message"] == "This is an Income prediction API!!"
 
 class TestPredict():
-    @pytest.fixture(scope="module")
-    def test_app():
-        client = TestClient(app)
-        return client
+
     @pytest.fixture
     def test_case_under_50k(self):
         body = ModelParams(
@@ -25,28 +26,28 @@ class TestPredict():
             "age": 39, 
             "workclass": "State-gov", 
             "fnlgt": 500, 
-            "education": " HS-grad", 
+            "education": "HS-grad", 
             "education-num": 9,
-           "marital-status": " Divorced", 
-           "occupation": "Adm-clerical", 
-           "relationship": "Not-in-family",
-           "race": "White", 
-           "sex": "Male",
-           "capital-gain": 0, 
-           "capital-loss": 0, 
-           "hours-per-week": 40,
-           "native-country": 
-           "United-States"})
+            "marital-status": "Divorced", 
+            "occupation": "Adm-clerical", 
+            "relationship": "Not-in-family",
+            "race": "White", 
+            "sex": "Male",
+            "capital-gain": 0, 
+            "capital-loss": 0, 
+            "hours-per-week": 40,
+            "native-country": "United-States"})
         return body
+    @pytest.fixture
     def test_case_over_50k(self):
         body = ModelParams(
         **{
             "age": 39, 
             "workclass": "State-gov", 
             "fnlgt": 80000, 
-            "education": " Masters", 
+            "education": "Masters", 
             "education-num": 14,
-           "marital-status": " Married-civ-spouse", 
+           "marital-status": "Married-civ-spouse", 
            "occupation": "Adm-clerical", 
            "relationship": "Not-in-family",
            "race": "White", 
@@ -54,19 +55,13 @@ class TestPredict():
            "capital-gain": 5000, 
            "capital-loss": 0, 
            "hours-per-week": 45,
-           "native-country": 
-           "United-States"})
+           "native-country": "United-States"})
         return body
     def test_predict_under_50k(self, test_case_under_50k, test_app):
-
         response = test_app.post("/predict", data=test_case_under_50k.json(by_alias=True))
-
         assert response.status_code == 200
         assert response.json()["prediction"] == "<=50K"
-
-    def test_predict_under_50k(self, test_case_over_50k, test_app):
-
+    def test_predict_over_50k(self, test_case_over_50k, test_app):
         response = test_app.post("/predict", data=test_case_over_50k.json(by_alias=True))
-
         assert response.status_code == 200
         assert response.json()["prediction"] == ">50K"
